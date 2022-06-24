d196cfaf1f64c40e84cf156d0bf819c4ec91cf35
Dickson-Pinheiro <62717698+Dickson-Pinheiro@users.noreply.github.com>
2022-06-24 00:41:31 -0300

    inicio do cabeçalho

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import {FiHelpCircle} from 'react-icons/fi'
 import {v4} from 'uuid'
 import { useState } from 'react'
 import Head from 'next/head'
@@ -82,10 +83,18 @@ const Home: NextPage = () => {
       <Head>
         <title>Anime Frame</title>
         <meta name="description" content="Anime Frame" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet"></link>
       </Head>
 
       <main className={styles.main}>
-      <h1>Anime Frame</h1>
+        <header>
+          <h1>Ani Frame</h1>
+        </header>
+        <div className={styles.containerHelpImageBullet}>
+        <div className={styles.containerHelp}>
+          <p>apoia.se/aniframe</p>
+          <FiHelpCircle size={40} color="#ffffff"/>
+        </div>
       <div className={styles.containerImageBullet}>
         <div className={styles.containerWrapperImage}>
           <img src={showImages[image].image} alt='image 1'/>
@@ -101,6 +110,7 @@ const Home: NextPage = () => {
               })
             }
         </div>
+      </div>
       </div>
        <div>
        <input type="text" onChange={e => setAnimeName(e.target.value)} value={animeName}/>
